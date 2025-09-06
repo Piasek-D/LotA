ac3187f0e3958d01042c1e6a4567bbe83845ce7e
Piasek-D <piasekd79@gmail.com>
2025-09-06 20:28:26 +0200

    Update azair_scraper.py

--- azair_scraper.py
+++ azair_scraper.py
@@ -1,22 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # tryb bez interfejsu
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+# Konfiguracja Chrome w trybie headless
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+# Uruchomienie WebDrivera
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.azair.eu/")
 
+# Czekamy na załadowanie strony
 time.sleep(5)
 
-airlines_element = driver.find_element(By.ID, "airlinesList")
-airlines = airlines_element.text.split('\n')
+# Pobieramy listę linii lotniczych
+try:
+    airlines_element = driver.find_element(By.ID, "airlinesList")
+    airlines = airlines_element.text.split('\n')
+except Exception as e:
+    airlines = ["Błąd podczas pobierania danych:", str(e)]
 
+# Tworzymy DataFrame
 df = pd.DataFrame(airlines, columns=["Linia lotnicza"])
+
+# Zapis do Excela
 df.to_excel("linie_lotnicze_azair.xlsx", index=False)
 
+# Zamykanie przeglądarki
 driver.quit()
